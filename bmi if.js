37e477b8c1c8264a7fb1cb353c7e9d9bee40450d
luusss <s1411222002@gmai.com>
2024-09-27 15:58:48 +0800

    0927

---
+++ bmi if.js
@@ -0,0 +1,31 @@
+
+if (bmi < 18.5) {
+    console.log('體重過輕');
+  } else if (18.5<=bmi<24) {
+    console.log('健康體位');
+  }
+  else if (24<=bmi<27) {
+    console.log('過重');
+  }
+  else if (27 <= bmi < 30) {
+    console.log=('輕度肥胖');
+  }else if (30 <= bmi < 35) {
+    console.log('中度肥胖');
+  }else if (bmi >= 35) {
+    console.log('重度肥胖');
+  }
+
+switch (bmi) {
+  case < 18.5:
+    console.log("體重過輕");
+    break;
+  case "Apples":
+    console.log("健康體位");
+    break;
+  case "Bananas":
+    console.log("Bananas are $0.48 a pound.");
+    break;
+  case "Cherries":
+    console.log("Cherries are $3.00 a pound.");
+    break;
+  } 
--- nodebmi.js
+++ nodebmi.js
@@ -1,8 +1,26 @@
 var readline = require("readline-sync");
 
 //input weight
-var weight =readline.question("Please input your weight(KG)?");
-var height =160;
+var weight =readline.questionInt("Please input your weight(KG)? ");
+var height =readline.questionInt("Please input your height(cm)? ");
 var bmi =weight/((height/100)**2)
 
-console.log("Hello! Your BMI value is"+bmi)
+console.log("Hello! Your BMI value is : "+bmi)
+
+if (bmi < 18.5) {
+    console.log('體重過輕');
+  } else if (18.5<=bmi<24) {
+    console.log('健康體重');
+  }
+  else if (24<=bmi<27) {
+    console.log('體重過重');
+  }
+  else if (27 <= bmi < 30) {
+    console.log=('輕度肥胖');
+  }else if (30 <= bmi < 35) {
+    console.log('中度肥胖');
+  }else if (bmi >= 35) {
+    console.log('重度肥胖');
+  }
+
+ 
