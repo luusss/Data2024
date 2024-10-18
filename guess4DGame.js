9617f7d40536a4ef34a76a97466fcae81c9a6785
luusss <s1411222002@gmail.com>
2024-10-18 13:41:01 +0800

    1018

--- guess4DGame.js
+++ guess4DGame.js
@@ -2,7 +2,7 @@ var readline = require("readline-sync");
 
 //Generate 4 digit randomly
 var Ans=[0,1,2,3,4,5,6,7,8,9]; // 一組 0-9 的數字
-for(var i=0;i<4;i++){
+for(var i=0;i<4;i++){  //迴圈執行 4 次
   var rand = Math.floor(Math.random()*10);// 產生 0 到 9 的隨機數字 // 交換 i 和 rand 位置上的數字
   var temp= Ans[i];
   Ans[i] = Ans[rand];
